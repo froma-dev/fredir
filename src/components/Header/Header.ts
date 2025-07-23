ca7d439e5cf345e938710ebafa287aef34131e93
froma <github@froma.dev>
2025-07-23 23:40:42 +0200

    add Header component

---
+++ src/components/Header/Header.ts
@@ -0,0 +1,23 @@
+import Blits from "@lightningjs/blits";
+import colors from "../../utils/colors";
+
+export default Blits.Component("Header", {
+  template: `
+    <Element
+      w="$width"
+      h="120"
+      color="$backgroundColor"
+    >
+      <Element src="$brand" w="90" h="90" x="30" y="12" />
+      <Text placement="{x: 'center', y: 'middle'}" content="$title" size="40" color="$fontColor" />
+    </Element>
+  `,
+  props: ["title", "brand"],
+  state() {
+    return {
+      backgroundColor: colors.gray[800],
+      fontColor: colors.gray[50],
+      width: this.$appState.w,
+    };
+  },
+});
